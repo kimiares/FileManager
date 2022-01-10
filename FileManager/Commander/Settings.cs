8c84db9d7f486bf2252154ffa365f8e4a06b3cca
kimiares <konstant.semionov@gmail.com>
2022-01-10 15:41:11 +0200

    test+settings

---
+++ FileManager/Commander/Settings.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace FileManager.Commander
+{
+    class Settings
+    {
+        public static Settings Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Settings();
+                return _instance;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public Configuration Sets;
+
+        protected Settings()
+        {
+            Sets = File.Exists("settings.xml") ? LoadSettings() : new Configuration();
+            SaveSettings();
+            InizialiseParams();
+        }
+
+
+        private int maxelementscolumn;
+        /// <summary>
+        /// Maximum count of elements in column
+        /// </summary>
+        public int MaxElementsColumn
+        {
+            get { return maxelementscolumn; }
+            set { maxelementscolumn = value; }
+        }
+        /// <summary>
+        /// Additional program settings, calculated on base main settings.  
+        /// </summary>
+        private void InizialiseParams()
+        {
+            MaxElementsColumn = Sets.PanelHeight - 2;
+        }
+
+
+        private static Settings _instance = null;
+
+        /// <summary>
+        /// Save settings to xml file 
+        /// </summary>
+        public void SaveSettings()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(Configuration));
+            using (FileStream fs = new FileStream("settings.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Sets);
+            }
+        }
+        /// <summary>
+        /// Load settings from xml file
+        /// </summary>
+        /// <returns></returns>
+        public Configuration LoadSettings()
+        {
+            Configuration NewConfigSettings = new Configuration();
+            XmlSerializer formatter = new XmlSerializer(typeof(Configuration));
+
+
+            using (FileStream fs = new FileStream("settings.xml", FileMode.OpenOrCreate))
+            {
+                NewConfigSettings = (Configuration)formatter.Deserialize(fs);
+            }
+            return NewConfigSettings;
+        }
+    }
+}
--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 ﻿using FileManager.Commander;
 using FileManager.Drawing;
 using System;
-
-=======
 ﻿using FileManager.Drawing;
 using FileManager.Structure;
 using FileManager.Structure.PanelStrategy;
@@ -12,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
->>>>>>> 94ecb01d8d3343a8e185d4ed9f10ff275c78b171
+
 
 namespace FileManager
 {
@@ -20,24 +18,15 @@ namespace FileManager
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Console.ResetColor();
            
             Settings MySet = Settings.Instance();
             Table m1 = new Table(MySet.Sets.PathLeft, new Drawing.Point( MySet.Sets.ALX, MySet.Sets.ALY), new Drawing.Point(MySet.Sets.BLX, MySet.Sets.BLY),4);
             Table m2 = new Table(MySet.Sets.PathRight, new Drawing.Point(MySet.Sets.ARX, MySet.Sets.ARY), new Drawing.Point(MySet.Sets.BRX, MySet.Sets.BRY), 4);
             Console.ReadKey();
-          
-=======
-            // Console.ResetColor();
-            ////  Line a = new Line(new Drawing.Point(0, 0, ' '), new Drawing.Point(0, 20, ' '));
-            //// a.Draw();
-            // Table m = new Table("sewfesdfsf", new Drawing.Point(0, 0), new Drawing.Point(60, 28),3);
-
-            // List<FileSystemInfo> input = Files.GetFiles(@"C:\");
 
 
-            // IStructure PANEL = new Panel<T>(new Drawing.Point(1, 1), new Drawing.Point(59, 27), "C:\\", m, new AllColumn<T>());
 
             Test test = new Test();
             test.TestMethod();
@@ -55,7 +44,7 @@ namespace FileManager
             {
 
 
-                //T cont = (T)Convert.ChangeType("content", typeof(T));
+                
                 List<string> input = new List<string>() { "A", "B", "C", "D", "E", "F" };
                 List<FileSystemInfo> testFSI = Operations.Files.GetFiles(@"C:\");
 
@@ -69,24 +58,11 @@ namespace FileManager
                     testFSI);
 
 
-                
-
-                //Button<string> b = new Button<string>(
-                //new Drawing.Point(1, 1),
-                //new Drawing.Point(59, 27),
-                //"content",
-                //new Table(
-                //    "BUTTON",
-                //    new Drawing.Point(1, 1),
-                //    new Drawing.Point(59, 27),
-                //    1));
-
-
 
             }
            
             
->>>>>>> 94ecb01d8d3343a8e185d4ed9f10ff275c78b171
+
 
         }
     }
