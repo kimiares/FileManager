126f4a0a5dcbfde3faada0a673ae04bfb5bc03dd
YuriiDDD <yurii_daniuk@jabil.com>
2022-02-01 10:47:38 +0200

    ok

---
+++ FileManager.Web/FileSystem.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.Web
+{
+    public class FileSystemModel
+    {
+        [JsonProperty(PropertyName ="FullName")]
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
